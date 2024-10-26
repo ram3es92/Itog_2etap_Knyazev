99cd2ac8412a4baaaa35c399a68b2e71d150a85b
Роман Князев <ram3es92@mail.ru>
2024-10-26 20:36:21 +0300

    В классе Хомяка реализованы функции нужные

--- mainprogramm/Hamster.java
+++ mainprogramm/Hamster.java
@@ -1,5 +1,25 @@
 package Itog_2etap_Knyazev.mainprogramm;
 
-public class Hamster {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hamster extends Animal {
+    private List<String> commands = new ArrayList<>();
+
+    public Hamster(String name, int age) {
+        super(name, age, "Hamster");
+        commands.add("Крутить колесо");
+        commands.add("Есть семечки");
+    }
+
+    @Override
+    public void showCommands() {
+        System.out.println("Команды хомяка: " + String.join(", ", commands));
+    }
+
+    @Override
+    public void learnCommand(String command) {
+        commands.add(command);
+        System.out.println("Хомяк выучил новую команду: " + command);
+    }
 }
