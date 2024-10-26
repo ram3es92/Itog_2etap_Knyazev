addd93fd3a0645a17cf13aeb698058770b4eb67b
Роман Князев <ram3es92@mail.ru>
2024-10-26 20:34:28 +0300

    В класс Кот добавлены функции и их реализации

--- mainprogramm/Cat.java
+++ mainprogramm/Cat.java
@@ -1,5 +1,25 @@
 package Itog_2etap_Knyazev.mainprogramm;
 
-public class Cat {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cat extends Animal {
+    private List<String> commands = new ArrayList<>();
+
+    public Cat(String name, int age) {
+        super(name, age, "Cat");
+        commands.add("Ловить мышь");
+        commands.add("Мяукать");
+    }
+
+    @Override
+    public void showCommands() {
+        System.out.println("Команды кота: " + String.join(", ", commands));
+    }
+
+    @Override
+    public void learnCommand(String command) {
+        commands.add(command);
+        System.out.println("Кот выучил новую команду: " + command);
+    }
 }
